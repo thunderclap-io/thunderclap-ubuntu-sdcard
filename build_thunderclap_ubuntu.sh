c05a9efffd4234c8c3dc0e738dbfd4918546fcb5
Theo Markettos <atm26@cl.cam.ac.uk>
2019-04-01 19:35:34 +0100

    Try to clean up loopback mounts before and after running

--- build_thunderclap_ubuntu.sh
+++ build_thunderclap_ubuntu.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+
+# try to clean up errant loopback mounts
+umount mnt/1 || true
+umount mnt/2 || true
+
 ./scripts/build_ubuntu_sdcard.sh  boards/enclustra-mercury-aa1-pe1 refdes system && \
 pxz sdimage.img
+
+# make sure everything is unmounted
+umount mnt/1 || true
+umount mnt/2 || true
