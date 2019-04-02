acde4cbef60c4c7be4dae5564799a809e759cfe7
Theo Markettos <atm26@cl.cam.ac.uk>
2019-04-02 15:13:44 +0100

    Try harder to unmount past loopback mounts

--- build_thunderclap_ubuntu.sh
+++ build_thunderclap_ubuntu.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+function umount_multiple
+{
+	DIR=$1
+	while umount $DIR ; do
+		echo "Unmounted $DIR"
+	done
+	true
+}
+
 # try to clean up errant loopback mounts
-umount mnt/1 || true
-umount mnt/2 || true
+umount_multiple mnt/1
+umount_multiple mnt/2
 
 ./scripts/build_ubuntu_sdcard.sh  boards/enclustra-mercury-aa1-pe1 refdes system && \
 pxz sdimage.img
 
 # make sure everything is unmounted
-umount mnt/1 || true
-umount mnt/2 || true
+umount_multiple mnt/1
+umount_multiple mnt/2
